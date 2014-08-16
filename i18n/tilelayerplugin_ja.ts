f635996ce6bff9778148f8eeecd5fe34daa2232a
Minoru Akagi <akaginch@gmail.com>
2014-08-16 16:37:30 +0900

    japanese translation

--- i18n/tilelayerplugin_ja.ts
+++ i18n/tilelayerplugin_ja.ts
@@ -4,137 +4,142 @@
 <context>
     <name>AddLayerDialog</name>
     <message>
-        <location filename="../addlayerdialog.py" line="41"/>
-        <source>Not set</source>
-        <translation>未設定</translation>
-    </message>
-    <message>
-        <location filename="../addlayerdialog.py" line="50"/>
+        <location filename="../addlayerdialog.py" line="48"/>
         <source>Title</source>
         <translation>タイトル</translation>
     </message>
     <message>
-        <location filename="../addlayerdialog.py" line="50"/>
+        <location filename="../addlayerdialog.py" line="48"/>
         <source>Url</source>
         <translation>URL</translation>
     </message>
     <message>
-        <location filename="../addlayerdialog.py" line="50"/>
+        <location filename="../addlayerdialog.py" line="48"/>
         <source>Zoom</source>
         <translation>ズーム</translation>
     </message>
     <message>
-        <location filename="../addlayerdialog.py" line="50"/>
+        <location filename="../addlayerdialog.py" line="48"/>
         <source>Extent</source>
         <translation>範囲</translation>
     </message>
     <message>
-        <location filename="../addlayerdialog.py" line="50"/>
+        <location filename="../addlayerdialog.py" line="48"/>
         <source>yOrigin</source>
         <translation>yOrigin</translation>
     </message>
     <message>
-        <location filename="../addlayerdialog.py" line="140"/>
-        <source>Select external layers directory</source>
-        <translation>外部レイヤ情報ディレクトリを選択して下さい</translation>
-    </message>
-    <message>
-        <location filename="../addlayerdialog.py" line="95"/>
-        <source>Fail to read: {}</source>
-        <translation>読み込みに失敗しました: {}</translation>
-    </message>
-    <message>
-        <location filename="../addlayerdialog.py" line="121"/>
+        <location filename="../addlayerdialog.py" line="123"/>
         <source>TileLayerPlugin</source>
         <translation>タイルレイヤプラグイン</translation>
     </message>
     <message>
-        <location filename="../addlayerdialog.py" line="121"/>
+        <location filename="../addlayerdialog.py" line="123"/>
         <source>Invalid line format: {} line {}</source>
         <translation>不正な行フォーマットです: {} line {}</translation>
     </message>
     <message>
-        <location filename="../addlayerdialog.py" line="50"/>
+        <location filename="../addlayerdialog.py" line="48"/>
         <source>Credit</source>
         <translation>著作権表記</translation>
+    </message>
+    <message>
+        <location filename="../addlayerdialog.py" line="94"/>
+        <source>Fail to read: {0} (encoding must be UTF-8)</source>
+        <translation>{0} の読み込みに失敗しました (文字コードはUTF-8にして下さい)</translation>
+    </message>
+    <message>
+        <location filename="../addlayerdialog.py" line="97"/>
+        <source>Fail to read: {0}</source>
+        <translation>{0} の読み込みに失敗しました</translation>
     </message>
 </context>
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="../ui_addlayerdialog.py" line="73"/>
+        <location filename="../addlayerdialog.ui" line="14"/>
         <source>Add tile layer</source>
         <translation>タイルレイヤを追加する</translation>
     </message>
     <message>
-        <location filename="../ui_settingsdialog.py" line="72"/>
+        <location filename="../settingsdialog.ui" line="36"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="../ui_addlayerdialog.py" line="77"/>
+        <location filename="../addlayerdialog.ui" line="64"/>
         <source>Add</source>
         <translation>追加</translation>
     </message>
     <message>
-        <location filename="../ui_addlayerdialog.py" line="78"/>
+        <location filename="../addlayerdialog.ui" line="74"/>
         <source>Close</source>
         <translation>閉じる</translation>
     </message>
     <message>
-        <location filename="../ui_propertiesdialog.py" line="99"/>
+        <location filename="../propertiesdialog.ui" line="120"/>
         <source>Properties</source>
         <translation>プロパティ</translation>
     </message>
     <message>
-        <location filename="../ui_propertiesdialog.py" line="94"/>
+        <location filename="../propertiesdialog.ui" line="38"/>
         <source>Style</source>
         <translation>スタイル</translation>
     </message>
     <message>
-        <location filename="../ui_propertiesdialog.py" line="95"/>
+        <location filename="../propertiesdialog.ui" line="49"/>
         <source>Transparency</source>
         <translation>透過度</translation>
     </message>
     <message>
-        <location filename="../ui_propertiesdialog.py" line="96"/>
+        <location filename="../propertiesdialog.ui" line="77"/>
         <source>Blending mode</source>
         <translation>混合モード</translation>
     </message>
     <message>
-        <location filename="../ui_addlayerdialog.py" line="74"/>
-        <source>External layers directory:</source>
-        <translation>外部レイヤ情報ディレクトリ:</translation>
-    </message>
-    <message>
-        <location filename="../ui_propertiesdialog.py" line="98"/>
+        <location filename="../propertiesdialog.ui" line="110"/>
         <source>Place the credit on the bottom right corner</source>
         <translation>右下にクレジット(著作権表記)を表示する</translation>
     </message>
     <message>
-        <location filename="../ui_propertiesdialog.py" line="97"/>
+        <location filename="../propertiesdialog.ui" line="89"/>
         <source>(Default: SourceOver)</source>
         <translation>(既定値: SourceOver)</translation>
     </message>
     <message>
-        <location filename="../ui_settingsdialog.py" line="70"/>
+        <location filename="../settingsdialog.ui" line="14"/>
         <source>TileLayerPlugin Settings</source>
         <translation>タイルレイヤプラグイン設定</translation>
     </message>
     <message>
-        <location filename="../ui_settingsdialog.py" line="71"/>
-        <source>External layers directory</source>
-        <translation>外部レイヤ情報ディレクトリ</translation>
-    </message>
-    <message>
-        <location filename="../ui_settingsdialog.py" line="73"/>
+        <location filename="../settingsdialog.ui" line="45"/>
         <source>Download time-out (sec)</source>
         <translation>ダウンロードのタイムアウト (秒)</translation>
     </message>
     <message>
-        <location filename="../ui_settingsdialog.py" line="74"/>
+        <location filename="../settingsdialog.ui" line="83"/>
         <source>Display navigation messages</source>
         <translation>ナビゲーションメッセージを表示する</translation>
+    </message>
+    <message>
+        <location filename="../addlayerdialog.ui" line="44"/>
+        <source>Settings</source>
+        <translation>設定</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="24"/>
+        <source>External layer definition directory</source>
+        <translation>外部レイヤ定義ディレクトリ</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="76"/>
+        <source>Move plugin to Layer menu/toolbar</source>
+        <translation>プラグインをレイヤメニュー/ツールバーに移動する</translation>
+    </message>
+    <message>
+        <location filename="../propertiesdialog.ui" line="103"/>
+        <source>Smooth rendering</source>
+        <translation>スムージング</translation>
     </message>
 </context>
 <context>
@@ -148,77 +153,77 @@
 <context>
     <name>TileLayer</name>
     <message>
-        <location filename="../tilelayer.py" line="142"/>
+        <location filename="../tilelayer.py" line="161"/>
         <source>Tile count is over limit ({0}, max={1})</source>
         <translation>タイル数が制限を超えています ({0}, 最大={1})</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="191"/>
+        <location filename="../tilelayer.py" line="219"/>
         <source>{0} files downloaded. {1} caches hit.</source>
         <translation>{0}ファイルダウンロード. {1}キャッシュヒット.</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="354"/>
+        <location filename="../tilelayer.py" line="379"/>
         <source> {} files failed.</source>
         <translation> {}ファイル失敗.</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="386"/>
+        <location filename="../tilelayer.py" line="412"/>
         <source>Title</source>
         <translation>タイトル</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="388"/>
+        <location filename="../tilelayer.py" line="414"/>
         <source>URL</source>
         <translation>URL</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="389"/>
+        <location filename="../tilelayer.py" line="415"/>
         <source>yOrigin</source>
         <translation>yOrigin</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="393"/>
+        <location filename="../tilelayer.py" line="419"/>
         <source>Not set</source>
         <translation>未設定</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="394"/>
+        <location filename="../tilelayer.py" line="420"/>
         <source>Zoom range</source>
         <translation>ズーム範囲</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="395"/>
+        <location filename="../tilelayer.py" line="421"/>
         <source>Layer Extent</source>
         <translation>レイヤ領域</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="135"/>
+        <location filename="../tilelayer.py" line="154"/>
         <source>Current zoom level ({0}) is smaller than zmin ({1}): {2}</source>
         <translation>現在のズームレベル({0})は最小ズームレベル({1})よりも小さいです: {2}</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="195"/>
+        <location filename="../tilelayer.py" line="223"/>
         <source>Download Timeout - {}</source>
         <translation>ダウンロードがタイムアウトしました - {}</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="199"/>
+        <location filename="../tilelayer.py" line="227"/>
         <source>Failed to download all {0} files. - {1}</source>
         <translation>全{0}ファイルのダウンロードに失敗しました. - {1}</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="352"/>
+        <location filename="../tilelayer.py" line="377"/>
         <source>{0} of {1} files downloaded.</source>
         <translation>{1}ファイルのうち{0}ファイルをダウンロードしました.</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="98"/>
+        <location filename="../tilelayer.py" line="116"/>
         <source>TileLayer is available in EPSG:3857</source>
         <translation>タイルレイヤはEPSG:3857で利用可能です</translation>
     </message>
     <message>
-        <location filename="../tilelayer.py" line="387"/>
+        <location filename="../tilelayer.py" line="413"/>
         <source>Credit</source>
         <translation>著作権表記</translation>
     </message>
@@ -226,19 +231,19 @@
 <context>
     <name>TileLayerPlugin</name>
     <message>
-        <location filename="../tilelayerplugin.py" line="61"/>
+        <location filename="../tilelayerplugin.py" line="65"/>
         <source>Add Tile Layer...</source>
         <translation>タイルレイヤを追加する...</translation>
     </message>
     <message>
-        <location filename="../tilelayerplugin.py" line="55"/>
+        <location filename="../tilelayerplugin.py" line="56"/>
         <source>TileLayerPlugin</source>
         <translation>タイルレイヤプラグイン</translation>
     </message>
     <message>
-        <location filename="../tilelayerplugin.py" line="64"/>
-        <source>Settings...</source>
-        <translation>設定...</translation>
+        <location filename="../tilelayerplugin.py" line="180"/>
+        <source>Project CRS has been changed to EPSG:3857.</source>
+        <translation>プロジェクトのCRSはEPSG:3857に変更されました.</translation>
     </message>
 </context>
 </TS>
